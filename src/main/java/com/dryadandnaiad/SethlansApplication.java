c0e0461d5fbaa228da9182da750de72543044f2b
Mario Estrella <mestrella@dryadandnaiad.com>
2017-03-09 01:42:00 -0600

    Initial project creation

---
+++ src/main/java/com/dryadandnaiad/SethlansApplication.java
@@ -0,0 +1,12 @@
+package com.dryadandnaiad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethlansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethlansApplication.class, args);
+	}
+}
