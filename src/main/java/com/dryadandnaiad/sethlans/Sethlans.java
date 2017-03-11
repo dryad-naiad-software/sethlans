98db6823a607f4ea0573b4a945d39be3d9ef2d92
Mario Estrella <mestrella@dryadandnaiad.com>
2017-03-10 20:16:58 -0600

    Command line options are back and filtered.

--- src/main/java/com/dryadandnaiad/sethlans/Sethlans.java
+++ src/main/java/com/dryadandnaiad/sethlans/Sethlans.java
@@ -18,13 +18,22 @@
 
 package com.dryadandnaiad.sethlans;
 
+import com.dryadandnaiad.sethlans.enums.SethlansMode;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionHandlerFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.logging.LogLevel;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created Mario Estrella on 3/9/17.
@@ -35,17 +44,71 @@ import java.awt.*;
 @SpringBootApplication
 public class Sethlans {
     private static final Logger LOG = LoggerFactory.getLogger(Sethlans.class);
+    // Command line options
+    @Option(name = "-help", aliases = "-h", usage = "Displays this screen\n", required = false, help = true, handler = com.dryadandnaiad.sethlans.utils.HelpOptionHandler.class)
+    private boolean help;
+    @Option(name = "-mode", usage = "Specify whether to operate as a server, node, or both(default)", required = false)
+    private SethlansMode mode = null;
+    @Option(name = "-loglevel", usage = "Sets the debug level for log file.  info: normal information messages(default), debug: turns on debug logging", required = false)
+    private LogLevel logLevel;
+    @Option(name = "-persist", usage = "Options passed via command line are saved and automatically used next startup", required = false)
+    private boolean persist;
+    @Option(name = "-http-port", usage = "Sets the http port for the WEB UI", metaVar = "7007", required = false)
+    private String httpPort = null;
+    @Option(name = "-https-port", usage = "Sets the https port for the WEB UI", metaVar = "7443", required = false)
+    private String httpsPort = null;
 
     public static void main(String[] args) {
+        new Sethlans().doMain(args);
+    }
+
+    public void doMain(String[] args) {
+        CmdLineParser cmdParser = new CmdLineParser(this);
+        List<String> arrayArgs = new ArrayList<String>();
+        try {
+            cmdParser.parseArgument(args);
+        } catch (CmdLineException e) {
+            LOG.error(e.getMessage());
+            System.err.println(e.getMessage());
+            System.err.println("Usage: ");
+            cmdParser.printUsage(System.err);
+            System.err.println();
+            System.err.println("Example: java -jar sethlans-x.x.jar -http-port 7007 -persist" + cmdParser.printExample(OptionHandlerFilter.REQUIRED));
+            return;
+        }
+
+        if (help) {
+            cmdParser.printUsage(System.out);
+            return;
+        }
+
+        if (httpPort != null) {
+            arrayArgs.add("--server.port=" + httpPort);
+        }
+
+        if (logLevel != null) {
+            arrayArgs.add("--logging.level.com.dryadandnaiad=" + logLevel);
+        }
+
+        if (persist) {
+            LOG.info("Saving command-line options to config file.");
+        }
+
+        String[] springArgs = new String[arrayArgs.size()];
+        springArgs = arrayArgs.toArray(springArgs);
+        startSpring(springArgs);
+    }
+
+
+    private void startSpring(String[] springArgs) {
         if (SystemTray.isSupported()) {
             SpringApplicationBuilder builder = new SpringApplicationBuilder(Sethlans.class);
             builder.headless(false);
-            builder.run(args);
+            builder.run(springArgs);
         } else {
-            SpringApplication.run(Sethlans.class, args);
+            SpringApplication.run(Sethlans.class, springArgs);
         }
-
-
     }
+
 }
 
