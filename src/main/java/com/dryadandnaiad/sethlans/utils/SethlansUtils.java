df49417ffe319ed8756b865f7370c2a185a4e9c3
Mario Estrella <mestrella@dryadandnaiad.com>
2017-03-12 08:32:19 -0500

    project should be using LF

--- src/main/java/com/dryadandnaiad/sethlans/utils/SethlansUtils.java
+++ src/main/java/com/dryadandnaiad/sethlans/utils/SethlansUtils.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2017. Dryad and Naiad Software LLC.
+ * Copyright (c) 2017 Dryad and Naiad Software LLC
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
