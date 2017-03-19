11f5c65f5e0c0d4e0dd6605fe80e9f98ea8fe6c9
Mario Estrella <mestrella@dryadandnaiad.com>
2017-03-19 05:41:24 -0500

    Auto copyright was on.

--- src/main/java/com/dryadandnaiad/sethlans/client/hardware/cpu/CPU.java
+++ src/main/java/com/dryadandnaiad/sethlans/client/hardware/cpu/CPU.java
@@ -1,10 +1,12 @@
+
 /*
- * Copyright (c) 2017 Dryad and Naiad Software LLC
+ * Copyright (C) 2010-2017 Laurent CLOUET
+ * Author Laurent CLOUET <laurent.clouet@nopnop.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * as published by the Free Software Foundation; version 2
+ * of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 package com.dryadandnaiad.sethlans.client.hardware.cpu;
