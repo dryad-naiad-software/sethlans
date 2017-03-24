894b6506510af5bdcc1d08ad95cffbab69898eb2
Mario Estrella <mestrella@dryadandnaiad.com>
2017-03-24 11:22:16 -0500

    working on download section

--- src/main/java/com/dryadandnaiad/sethlans/services/network/BlenderDownloadServiceImpl.java
+++ src/main/java/com/dryadandnaiad/sethlans/services/network/BlenderDownloadServiceImpl.java
@@ -26,9 +26,17 @@ import com.dryadandnaiad.sethlans.services.database.BlenderFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 
@@ -43,8 +51,9 @@ public class BlenderDownloadServiceImpl implements BlenderDownloadService {
     private static final Logger LOG = LoggerFactory.getLogger(BlenderDownloadServiceImpl.class);
     private BlenderFileService blenderFileService;
     private List<BlenderFile> blenderFiles;
-
     private List<BlenderObject> blenderBinaries;
+    @Value("${sethlans.blenderDir}")
+    private String downloadLocation;
 
     @Autowired
     public void setBlenderFileService(BlenderFileService blenderFileService) {
@@ -54,16 +63,69 @@ public class BlenderDownloadServiceImpl implements BlenderDownloadService {
     @Override
     @Async
     public Future<Boolean> downloadRequestedBlenderFiles() {
-        LOG.debug("Called");
-        blenderBinaries = BlenderUtils.listBinaries();
         blenderFiles = (List<BlenderFile>) blenderFileService.listAll();
-        for (BlenderFile blenderFile : blenderFiles) {
+        blenderBinaries = BlenderUtils.listBinaries();
+        List<BlenderFile> blenderDownloadList = prepareDownload();
+
+
+        for (BlenderFile blenderFile : blenderDownloadList) {
             LOG.debug(blenderFile.toString());
-        }
-        for (BlenderObject blenderBinary : blenderBinaries) {
-            LOG.debug(blenderBinary.toString());
+            File saveLocation = new File(downloadLocation + File.separator + "binaries" + File.separator);
+            saveLocation.mkdirs();
+            LOG.debug(blenderFile.getBlenderVersion());
+            LOG.debug(saveLocation.toString());
+            URL url = null;
+            HttpURLConnection connection = null;
+            try {
+                url = new URL(blenderFile.getBlenderFile());
+                connection = (HttpURLConnection) url.openConnection();
+                InputStream stream = connection.getInputStream();
+                //Files.copy(stream, Paths.get(saveLocation + "test.zip"));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+            }
+
+
         }
 
+
+        LOG.debug(downloadLocation);
+
         return null;
+    }
+
+    private List<BlenderFile> prepareDownload() {
+        List<BlenderFile> list = new ArrayList<>();
+        for (BlenderFile blenderFile : blenderFiles) {
+            if (!blenderFile.isDownloaded()) {
+                for (BlenderObject blenderBinary : blenderBinaries) {
+                    if (blenderBinary.getBlenderVersion().equals(blenderFile.getBlenderVersion())) {
+                        switch (blenderFile.getBlenderBinaryOS().toLowerCase()) {
+                            case "windows32":
+                                blenderFile.setBlenderFile(blenderBinary.getWindows32());
+                                break;
+                            case "windows64":
+                                blenderFile.setBlenderFile(blenderBinary.getWindows64());
+                                break;
+                            case "macos":
+                                blenderFile.setBlenderFile(blenderBinary.getMacOS());
+                                break;
+                            case "linux64":
+                                blenderFile.setBlenderFile(blenderBinary.getLinux64());
+                                break;
+                            case "linux32":
+                                blenderFile.setBlenderFile(blenderBinary.getLinux32());
+
+                        }
+                    }
+                }
+                list.add(blenderFile);
+            }
+        }
+        return list;
     }
 }
