63053d107595e6820dd284d57de3205e68f8883c
Mario Estrella <mestrella@dryadandnaiad.com>
2017-10-29 20:35:26 -0500

    Foundation for Node Addition and Discovery

---
+++ src/main/java/com/dryadandnaiad/sethlans/services/database/SethlansNodeService.java
@@ -0,0 +1,10 @@
+package com.dryadandnaiad.sethlans.services.database;
+
+/**
+ * Created Mario Estrella on 10/29/17.
+ * Dryad and Naiad Software LLC
+ * mestrella@dryadandnaiad.com
+ * Project: sethlans
+ */
+public interface SethlansNodeService {
+}
---
+++ src/main/java/com/dryadandnaiad/sethlans/services/network/NodeDiscoveryService.java
@@ -0,0 +1,10 @@
+package com.dryadandnaiad.sethlans.services.network;
+
+/**
+ * Created Mario Estrella on 10/29/17.
+ * Dryad and Naiad Software LLC
+ * mestrella@dryadandnaiad.com
+ * Project: sethlans
+ */
+public interface NodeDiscoveryService {
+}
