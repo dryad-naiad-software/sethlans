c946979a67c48c31173c784a3c972d3156d0c6fc
Mario Estrella <mestrella@dryadandnaiad.com>
2017-11-23 01:24:59 -0600

    Updating copyright on all files

--- src/main/java/com/dryadandnaiad/sethlans/controllers/AbstractSethlansController.java
+++ src/main/java/com/dryadandnaiad/sethlans/controllers/AbstractSethlansController.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2017 Dryad and Naiad Software LLC.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
 package com.dryadandnaiad.sethlans.controllers;
 
 import com.dryadandnaiad.sethlans.enums.SethlansMode;
@@ -73,8 +92,7 @@ public class AbstractSethlansController implements ApplicationListener<SethlansE
         notification = event.isNewNotification();
         if(notification) {
             notificationMessage.add(event.getMessage());
-        }
-        else {
+        } else {
             notificationMessage = new ArrayList<>();
         }
 
