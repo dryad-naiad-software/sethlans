d7164ef931e02867553903e4083722e737560272
Mario Estrella <mestrella@dryadandnaiad.com>
2017-12-21 02:58:55 -0600

    Initial work on Node settings configuration

--- src/main/java/com/dryadandnaiad/sethlans/controllers/AbstractSethlansController.java
+++ src/main/java/com/dryadandnaiad/sethlans/controllers/AbstractSethlansController.java
@@ -20,6 +20,7 @@
 package com.dryadandnaiad.sethlans.controllers;
 
 import com.dryadandnaiad.sethlans.domains.database.events.SethlansNotification;
+import com.dryadandnaiad.sethlans.enums.ComputeType;
 import com.dryadandnaiad.sethlans.enums.SethlansMode;
 import com.dryadandnaiad.sethlans.events.SethlansEvent;
 import com.dryadandnaiad.sethlans.services.database.NotificationDatabaseService;
@@ -51,6 +52,9 @@ abstract public class AbstractSethlansController implements ApplicationListener<
     @Value("${sethlans.mode}")
     private SethlansMode mode;
 
+    @Value("${sethlans.computeMethod}")
+    private ComputeType computeType;
+
     @Value("${sethlans.firsttime}")
     private boolean firstTime;
 
@@ -64,6 +68,11 @@ abstract public class AbstractSethlansController implements ApplicationListener<
     @ModelAttribute("sethlansmode")
     public String getMode() {
         return mode.toString();
+    }
+
+    @ModelAttribute("computetype")
+    public String getComputeType() {
+        return computeType.toString();
     }
 
     @ModelAttribute("username")
