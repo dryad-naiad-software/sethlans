b7b160cb2c33b75842bbf628400241abd76d8dfb
Mario Estrella <mestrella@dryadandnaiad.com>
2018-01-15 20:18:05 -0600

    Drastically reduced HHH000010: On release of batch it still contained JDBC statements. Attempting for a larger window

--- src/main/java/com/dryadandnaiad/sethlans/controllers/ServerProjectRestController.java
+++ src/main/java/com/dryadandnaiad/sethlans/controllers/ServerProjectRestController.java
@@ -160,7 +160,7 @@ public class ServerProjectRestController {
                 // For busy environments with lots of nodes this prevents the server from getting overwhelmed.
                 Integer randomSleep;
                 Random r = new Random();
-                randomSleep = r.nextInt(20000 - 5000) + 5000;
+                randomSleep = r.nextInt(30000 - 5000) + 5000;
                 LOG.debug("Render Task received from " + hostname + " throttling for " + randomSleep + " milliseconds");
                 Thread.sleep(randomSleep);
 
@@ -168,7 +168,7 @@ public class ServerProjectRestController {
                 // Additional check to avoid writing to the same project at the same time.
                 LOG.debug("Checking to see if project is in use.");
                 while (blenderProjectDatabaseService.isProjectDBEntryInUse(project_uuid)) {
-                    randomSleep = r.nextInt(20000 - 5000) + 5000;
+                    randomSleep = r.nextInt(30000 - 5000) + 5000;
                     Thread.sleep(randomSleep);
                     LOG.debug("Project in use, sleeping for " + randomSleep);
 
