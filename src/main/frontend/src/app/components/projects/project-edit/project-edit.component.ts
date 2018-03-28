39f92ba352583aee587e396d72f9e8ee193a8e97
Mario Estrella <mestrella@dryadandnaiad.com>
2018-03-28 00:27:32 -0500

    Starting Project Edit component

--- src/main/frontend/src/app/components/projects/project-edit/project-edit.component.ts
+++ src/main/frontend/src/app/components/projects/project-edit/project-edit.component.ts
@@ -18,6 +18,13 @@
  */
 
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {ComputeMethod} from "../../../enums/compute.method.enum";
+import {RenderOutputFormat} from "../../../enums/render_output_format.enum";
+import {Project} from "../../../models/project.model";
+import {BlenderEngine} from "../../../enums/blender_engine.enum";
+import {ProjectType} from "../../../enums/project_type.enum";
 
 @Component({
   selector: 'app-project-edit',
@@ -25,11 +32,55 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./project-edit.component.scss']
 })
 export class ProjectEditComponent implements OnInit {
+  projectDetails: Project;
+  projectTypes = ProjectType;
+  availableBlenderVersions: any[];
+  projectLoaded: boolean = false;
+  useParts: boolean = true;
+  formats = RenderOutputFormat;
+  computeMethods = ComputeMethod;
+  engines = BlenderEngine;
+  status: number = 0;
 
-  constructor() {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  loadProjectDetails(event) {
+    let response: any = JSON.parse(event.xhr.response);
+    this.projectDetails = <Project>response;
+    if (this.projectDetails.projectType == this.projectTypes.STILL_IMAGE) {
+      this.projectDetails.endFrame = 1;
+      this.projectDetails.stepFrame = 1;
+    }
+    if (this.projectDetails.selectedBlenderversion == null) {
+      this.projectDetails.selectedBlenderversion = this.availableBlenderVersions[0];
+    }
+    console.log(this.projectDetails);
+    this.projectLoaded = true;
+  }
+
+  getAvailableBlenderVersions() {
+    this.http.get('/api/info/installed_blender_versions')
+      .subscribe(
+        (blenderVersions: any[]) => {
+          this.availableBlenderVersions = blenderVersions;
+        });
+  }
+
+  projectConfigNext() {
+    this.status = this.status + 1;
+  }
+
+  projectConfigPrevious() {
+    this.status = this.status - 1;
+  }
+
+  returnToProjects(): void {
+    this.router.navigateByUrl("/projects").then(() => location.reload());
+    ;
   }
 
 }
