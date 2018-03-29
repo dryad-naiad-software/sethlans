da578414fe7e3cf0ad1f69d436644e358d6dcb83
Mario Estrella <mestrella@dryadandnaiad.com>
2018-03-29 15:08:38 -0500

    Problems with image compiling

--- src/main/java/com/dryadandnaiad/sethlans/converters/ProjectFormToBlenderProject.java
+++ src/main/java/com/dryadandnaiad/sethlans/converters/ProjectFormToBlenderProject.java
@@ -22,13 +22,25 @@ package com.dryadandnaiad.sethlans.converters;
 import com.dryadandnaiad.sethlans.domains.database.blender.BlenderProject;
 import com.dryadandnaiad.sethlans.forms.ProjectForm;
 import com.dryadandnaiad.sethlans.services.database.SethlansUserDatabaseService;
+import com.dryadandnaiad.sethlans.utils.RandomString;
+import com.google.common.base.Throwables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @Component
 public class ProjectFormToBlenderProject implements Converter<ProjectForm, BlenderProject> {
     private SethlansUserDatabaseService sethlansUserDatabaseService;
+
+    @Value("${sethlans.projectDir}")
+    private String projectDir;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectFormToBlenderProject.class);
 
     @Override
     public BlenderProject convert(ProjectForm projectForm) {
@@ -52,6 +64,28 @@ public class ProjectFormToBlenderProject implements Converter<ProjectForm, Blend
         blenderProject.setResPercentage(projectForm.getResPercentage());
         blenderProject.setBlendFilename(projectForm.getUploadedFile());
         blenderProject.setPartsPerFrame(projectForm.getPartsPerFrame());
+
+
+        RandomString randomString = new RandomString(6);
+        File blenderProjectDirectory = new File(projectDir + File.separator + projectForm.getProjectName().replaceAll(" ", "_").toLowerCase() + "_" + randomString.nextString());
+        try {
+            if (!blenderProjectDirectory.mkdirs()) {
+                throw new Exception("Unable to create directory " + blenderProjectDirectory.toString());
+            }
+            File projecttemp = new File(projectForm.getFileLocation());
+            if (projecttemp.renameTo(new File(blenderProjectDirectory + File.separator + projectForm.getUploadedFile()))) {
+                LOG.debug(projectForm.getUploadedFile() + " moved to " + blenderProjectDirectory.toString());
+                blenderProject.setBlendFileLocation(blenderProjectDirectory + File.separator + projectForm.getUploadedFile());
+                blenderProject.setProjectRootDir(blenderProjectDirectory.toString());
+            } else {
+                throw new Exception(projectForm.getUploadedFile() + " failed to move");
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            LOG.error(Throwables.getStackTraceAsString(e));
+            return null;
+        }
+
         return blenderProject;
     }
 
