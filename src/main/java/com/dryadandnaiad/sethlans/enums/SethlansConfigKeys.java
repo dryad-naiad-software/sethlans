5f8c6e1c45f24fa96015b6e076c9472f7d607594
Mario Estrella <mestrella@dryadandnaiad.com>
2018-04-06 11:41:54 -0500

    Initial work towards ffmpeg

--- src/main/java/com/dryadandnaiad/sethlans/enums/SethlansConfigKeys.java
+++ src/main/java/com/dryadandnaiad/sethlans/enums/SethlansConfigKeys.java
@@ -37,6 +37,7 @@ public enum SethlansConfigKeys {
     BINARY_DIR("sethlans.binDir"),
     TEMP_DIR("sethlans.tempDir"),
     CACHE_DIR("sethlans.cacheDir"),
+    FFMPEG_BIN("sethlans.ffmpeg.binary"),
     CACHED_BLENDER_BINARIES("sethlans.cachedBlenderBinaries"),
     GPU_DEVICE("sethlans.gpu_id"),
     CPU_CORES("sethlans.cores"),
