1e77ddaa4464518ad288b9565b977002d9e456cd
Mario Estrella <mestrella@dryadandnaiad.com>
2018-07-23 21:04:31 -0500

    user management reintroduced

--- src/main/ui/src/app/components/user-settings/user-settings.component.ts
+++ src/main/ui/src/app/components/user-settings/user-settings.component.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018 Dryad and Naiad Software LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {UserInfo} from '../../models/userinfo.model';
@@ -25,6 +44,7 @@ export class UserSettingsComponent implements OnInit {
   ngOnInit() {
     this.getUserInfo();
     this.passFields = new PasswordSet();
+    document.body.style.background = 'rgba(0, 0, 0, .6)';
   }
 
   getUserInfo() {
