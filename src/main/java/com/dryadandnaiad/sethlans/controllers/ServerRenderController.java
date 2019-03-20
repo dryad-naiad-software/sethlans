954698cb9ab649e0c874cb12022a9138383e8a68
Mario Estrella <mestrella@dryadandnaiad.com>
2019-03-20 10:58:14 -0500

    Making sure project is not null before doing checks

--- src/main/java/com/dryadandnaiad/sethlans/controllers/ServerRenderController.java
+++ src/main/java/com/dryadandnaiad/sethlans/controllers/ServerRenderController.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2018 Dryad and Naiad Software LLC
+ * Copyright (c) 2019 Dryad and Naiad Software LLC
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
+ *  
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -180,25 +180,30 @@ public class ServerRenderController {
             if (!part.isEmpty()) {
                 SethlansNode sethlansNode = sethlansNodeDatabaseService.getByConnectionUUID(connection_uuid);
                 BlenderProject blenderProject = blenderProjectDatabaseService.getByProjectUUIDWithoutFrameParts(project_uuid);
-                if (!blenderProject.isUserStopped()) {
-                    LOG.info("Received response from " + sethlansNode.getHostname() + ", adding to processing Queue.");
-                    try {
-                        File receivedFile = new File(blenderProject.getProjectRootDir() + File.separator + "received" + File.separator + StringUtils.left(queue_uuid, 8) + "-" + System.currentTimeMillis() + ".png");
-                        part.transferTo(receivedFile);
-                        ProcessQueueItem processQueueItem = new ProcessQueueItem();
-                        processQueueItem.setConnectionUUID(connection_uuid);
-                        processQueueItem.setPart(receivedFile.toString());
-                        LOG.info("Received file from " + sethlansNode.getHostname() + " saved as " + receivedFile.toString());
-                        processQueueItem.setQueueUUID(queue_uuid);
-                        processQueueItem.setProjectUUID(project_uuid);
-                        processQueueItem.setRenderTime(render_time);
-                        queueService.addItemToProcess(processQueueItem);
-                    } catch (IOException e) {
-                        LOG.error(Throwables.getStackTraceAsString(e));
+                if (blenderProject != null) {
+                    if (!blenderProject.isUserStopped()) {
+                        LOG.info("Received response from " + sethlansNode.getHostname() + ", adding to processing Queue.");
+                        try {
+                            File receivedFile = new File(blenderProject.getProjectRootDir() + File.separator + "received" + File.separator + StringUtils.left(queue_uuid, 8) + "-" + System.currentTimeMillis() + ".png");
+                            part.transferTo(receivedFile);
+                            ProcessQueueItem processQueueItem = new ProcessQueueItem();
+                            processQueueItem.setConnectionUUID(connection_uuid);
+                            processQueueItem.setPart(receivedFile.toString());
+                            LOG.info("Received file from " + sethlansNode.getHostname() + " saved as " + receivedFile.toString());
+                            processQueueItem.setQueueUUID(queue_uuid);
+                            processQueueItem.setProjectUUID(project_uuid);
+                            processQueueItem.setRenderTime(render_time);
+                            queueService.addItemToProcess(processQueueItem);
+                        } catch (IOException e) {
+                            LOG.error(Throwables.getStackTraceAsString(e));
+                        }
+                    } else {
+                        LOG.info("Project stopped, discarding response");
                     }
                 } else {
-                    LOG.info("Project stopped, discarding response");
+                    LOG.info("Project does not exist.");
                 }
+                
                 
             }
         }
