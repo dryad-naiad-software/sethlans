93d6c280c46d9d1a5f0312e8d45b87a40547afd9
Mario Estrella <mestrella@dryadandnaiad.com>
2019-03-31 20:44:23 -0500

    Need to account for first time setup for systray

--- src/main/ui/src/app/components/setup/get-started-wizard/wizard-add-nodes/wizard-add-nodes.component.ts
+++ src/main/ui/src/app/components/setup/get-started-wizard/wizard-add-nodes/wizard-add-nodes.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Dryad and Naiad Software LLC
+ * Copyright (c) 2019 Dryad and Naiad Software LLC
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -58,8 +58,11 @@ export class WizardAddNodesComponent implements OnInit {
           value.active = false;
         }
         if (idx === array.length - 1) {
-          this.getStartedWizardForm.scanComplete = true;
-          this.refreshList();
+          var wizard = this;
+          setTimeout(function () {
+            wizard.refreshList();
+            this.getStartedWizardForm.scanComplete = true;
+          }, 1000);
         }
       });
     });
