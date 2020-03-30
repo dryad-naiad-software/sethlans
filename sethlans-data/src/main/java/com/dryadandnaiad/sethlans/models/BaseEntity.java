1eaf852093b44b1432b59fbde808483ac1dc6b2a
Mario Estrella <mestrella@dryadandnaiad.com>
2020-03-29 21:32:17 -0500

    More work on modules

--- sethlans-data/src/main/java/com/dryadandnaiad/sethlans/models/BaseEntity.java
+++ sethlans-data/src/main/java/com/dryadandnaiad/sethlans/models/BaseEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -18,8 +19,12 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Version
     private Long version;
     private Date dateCreated;
     private Date lastUpdated;
