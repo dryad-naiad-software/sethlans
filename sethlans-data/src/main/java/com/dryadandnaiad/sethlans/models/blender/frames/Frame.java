b3b8e6d6b9dc5c76e178e0e39240857769f90211
Mario Estrella <mestrella@dryadandnaiad.com>
2020-04-08 03:35:11 -0500

    moving to mongodb from h2

--- sethlans-data/src/main/java/com/dryadandnaiad/sethlans/models/blender/frames/Frame.java
+++ sethlans-data/src/main/java/com/dryadandnaiad/sethlans/models/blender/frames/Frame.java
@@ -21,7 +21,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
@@ -35,7 +34,6 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
 public class Frame {
     private String frameFileName;
     private int frameNumber;
