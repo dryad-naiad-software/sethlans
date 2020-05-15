7aca7b80b86cb659e5a41e0e44d98a36bf1dbe3c
Mario Estrella <mestrella@dryadandnaiad.com>
2020-05-15 14:30:26 -0500

    Initial work on Setup

---
+++ sethlans-main/src/main/java/com/dryadandnaiad/sethlans/models/settings/MailSettings.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020. Dryad and Naiad Software LLC.
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.dryadandnaiad.sethlans.models.settings;
+
+import lombok.Data;
+
+/**
+ * File created by Mario Estrella on 5/15/2020.
+ * Dryad and Naiad Software LLC
+ * mestrella@dryadandnaiad.com
+ * Project: sethlans
+ */
+@Data
+public class MailSettings {
+    private String mailHost;
+    private String mailPort;
+    private String username;
+    private String password;
+    private String replyToAddress;
+    private boolean mailEnabled;
+    private boolean smtpAuth;
+    private boolean sslEnabled;
+    private boolean startTLSEnabled;
+    private boolean startTLSRequired;
+}
