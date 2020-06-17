3b2519c0c998f4c560b41e609f34224a5d94e8f6
Mario Estrella <mestrella@dryadandnaiad.com>
2020-06-16 21:40:53 -0500

    Initial work on AdminNodeController Tests, updated db schema

---
+++ src/test/java/com/dryadandnaiad/sethlans/controllers/AdminNodeControllerTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2020. Dryad and Naiad Software LLC.
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.dryadandnaiad.sethlans.controllers;
+
+import com.dryadandnaiad.sethlans.enums.ConfigKeys;
+import com.dryadandnaiad.sethlans.enums.LogLevel;
+import com.dryadandnaiad.sethlans.enums.NodeType;
+import com.dryadandnaiad.sethlans.enums.SethlansMode;
+import com.dryadandnaiad.sethlans.models.forms.SetupForm;
+import com.dryadandnaiad.sethlans.models.settings.MailSettings;
+import com.dryadandnaiad.sethlans.models.settings.NodeSettings;
+import com.dryadandnaiad.sethlans.models.system.Server;
+import com.dryadandnaiad.sethlans.utils.ConfigUtils;
+import com.dryadandnaiad.sethlans.utils.PropertiesUtils;
+import com.dryadandnaiad.sethlans.utils.QueryUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * File created by Mario Estrella on 6/16/2020.
+ * Dryad and Naiad Software LLC
+ * mestrella@dryadandnaiad.com
+ * Project: sethlans
+ */
+@ActiveProfiles("DUAL")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "classpath:sethlans.properties")
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+class AdminNodeControllerTest {
+
+    static File SETHLANS_DIRECTORY = new File(SystemUtils.USER_HOME + File.separator + ".sethlans");
+
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        var setupSettings = SetupForm.builder()
+                .appURL("https://localhost:7443")
+                .ipAddress(QueryUtils.getIP())
+                .logLevel(LogLevel.DEBUG)
+                .mode(SethlansMode.DUAL)
+                .port("7443").build();
+        var nodeSettings = NodeSettings.builder().nodeType(NodeType.CPU).tileSizeCPU(32).cores(4).build();
+        PropertiesUtils.writeNodeSettings(nodeSettings);
+        PropertiesUtils.writeSetupSettings(setupSettings);
+        PropertiesUtils.writeDirectories(SethlansMode.DUAL);
+        var mailSettings = MailSettings.builder()
+                .mailEnabled(false)
+                .build();
+        PropertiesUtils.writeMailSettings(mailSettings);
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        FileSystemUtils.deleteRecursively(SETHLANS_DIRECTORY);
+    }
+
+
+    @Test
+    void addServer() throws Exception {
+        var server = Server.builder()
+                .ipAddress(QueryUtils.getIP())
+                .systemID(ConfigUtils.getProperty(ConfigKeys.SYSTEM_ID))
+                .hostname(QueryUtils.getHostname())
+                .networkPort(ConfigUtils.getProperty(ConfigKeys.HTTPS_PORT))
+                .build();
+        var objectMapper = new ObjectMapper();
+        var serverJson = objectMapper.writeValueAsString(server);
+        mvc.perform(post("/api/v1/management/add_server")
+                .contentType(MediaType.APPLICATION_JSON).content(serverJson))
+                .andExpect(status().isCreated());
+    }
+}
