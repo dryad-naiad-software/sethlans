8bb9132ed5f36b6544a7d35920115deb7b7e9f23
Mario Estrella <mestrella@dryadandnaiad.com>
2021-07-23 22:21:53 -0500

    Initial user should be a super administrator and it should be active.  updated setup service to make sure to override submitted user

--- src/main/java/com/dryadandnaiad/sethlans/config/SecurityConfig.java
+++ src/main/java/com/dryadandnaiad/sethlans/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.dryadandnaiad.sethlans.utils.PropertiesUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -16,9 +15,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
-        log.info("Configure");
         if (!PropertiesUtils.isFirstTime()) {
-            log.info("Not First Time");
             http.authorizeRequests(authorize -> {
                 authorize.antMatchers("/api/v1/info/version").permitAll();
             });
@@ -28,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             http.formLogin();
             http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
         } else {
-            log.info("first time");
+
             http.authorizeRequests().antMatchers("/*").permitAll();
             http.csrf().disable();
         }
