7e8f4b75cb4c78de39cd27c93e75b2cf982f7df5
Mario Estrella <mestrella@dryadandnaiad.com>
2021-09-14 23:42:33 -0500

    ignore csrf on api

--- src/main/java/com/dryadandnaiad/sethlans/config/SecurityConfig.java
+++ src/main/java/com/dryadandnaiad/sethlans/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Slf4j
 @Configuration
@@ -24,8 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/v1/management/**").hasAnyAuthority(Role.ADMINISTRATOR.toString(), Role.SUPER_ADMINISTRATOR.toString())
                     .anyRequest().authenticated());
             http.formLogin();
-            http.csrf().disable();
-            //http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+            http
+                    .csrf(csrf ->
+                            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                    .ignoringAntMatchers("/api/v1/**"));
         } else {
 
             http.authorizeRequests().antMatchers("/*").permitAll();
