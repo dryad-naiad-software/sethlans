e2d8a3a10abeee2b97aef358db211489b46adde7
Mario Estrella <mestrella@dryadandnaiad.com>
2022-05-22 20:36:58 -0500

    More work on setup wizard ui tests

--- src/main/ui/cypress/integration/sethlans.setup.spec.ts
+++ src/main/ui/cypress/integration/sethlans.setup.spec.ts
@@ -8,7 +8,18 @@ describe('Sethlans Setup Wizard Tests', () => {
     cy.get('[id=next]').click()
     cy.get('[id=username]').type("sampleuser")
     cy.get('[id=password]').type("test1234")
+    cy.get('[id=email]').type("sampleuser@example.com")
     cy.get('[id=passwordConfirm]').type("test1234")
+    cy.get('[id=challenge1]').select(0)
+    cy.get('[id=response1]').type("Something Interesting Part 1")
+    cy.get('[id=challenge2]').select(2)
+    cy.get('[id=response2]').type("Something Boring")
+    cy.get('[id=challenge3]').select(3)
+    cy.get('[id=response3]').type("simple")
+    cy.get('[id=next]').click()
+    cy.get('[id=blenderVersion]').select(0)
+    cy.get('[id=next]').click()
+
 
   });
 
